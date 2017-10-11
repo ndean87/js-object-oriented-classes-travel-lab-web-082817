76331410e1e06276b0f314abd0498bc6eab4f30e
ndean87 <ndean87@gmail.com>
2017-10-11 11:00:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear() - 1;
+  }
+}
+
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let westEast = Math.abs(
+      eastWest.indexOf(this.endingLocation.horizontal) -
+        eastWest.indexOf(this.beginningLocation.horizontal)
+    );
+    let northSouth = Math.abs(
+      this.endingLocation.vertical - this.beginningLocation.vertical
+    );
+    return northSouth + westEast;
+  }
+
+  estimatedTime(peak) {
+    if (!peak) {
+      return this.blocksTravelled() / 3;
+    } else {
+      return this.blocksTravelled() / 2;
+    }
+  }
+}
